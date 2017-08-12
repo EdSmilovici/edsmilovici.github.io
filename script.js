451e66867c05b5974fd7fdc48048127ab947a93a
Ed Smilovici <edsmilovici@gmail.com>
2017-08-11 20:08:22 -0400

    Refs #4 Add the ability to click on the card selectors and switch the visibility - Added event listeners to change selected card on click - some minor html refactoring notably: converted all the div with the card id to have class id and changed the card ids to be their respective selector.id+”Card”

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-
 function sizeSelector() {
 	var height = window.innerHeight - window.innerWidth;
 	document.getElementById("cardSelector").style.height = height+"px";
+}
+
+function showCard(){
+	var currentlyVisibleCard = document.getElementsByClassName('card selectedCard');
+	var newlyVisibleCard = document.getElementById(this.id + 'Card');
+	Array.prototype.forEach.call(currentlyVisibleCard, function(card){
+		card.classList.remove('selectedCard');
+	});
+	newlyVisibleCard.classList.add('selectedCard');
+}
+
+function addEventListeners() {
+	var selectors = document.getElementsByClassName('selector');
+	Array.prototype.forEach.call(selectors, function(selector){
+		selector.addEventListener("click", showCard);
+	});
+}
+
+function runPostLoadFunctions(){
+	sizeSelector();
+	addEventListeners();
 }
