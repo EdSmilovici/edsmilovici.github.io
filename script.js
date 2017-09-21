9d53198b2d63b1d1a4305ba44b9b799e4ae043e9
Ed Smilovici <edsmilovici@gmail.com>
2017-09-20 21:15:29 -0400

    Refs #12 Investigate viability of new layout strategy - added cardviewplaceholder - styled cardView to stay fixed at the top and be position fixed - removed js that set width and height of selector and replaced it with css - well i think we’re back at the jQueryless dream because jQuery was only being used in the removed function

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-$(window).ready(runPostLoadFunctions);
-
-function sizeSelector() {
-	var viewportHeight = $(window).height();
-	var viewportWidth = $(window).width();
-	var height = viewportWidth - viewportWidth;
-	var cardSelector = $("#cardSelector");
-	cardSelector.css('height', height+"px");
-    if (viewportWidth/viewportHeight >= 17500/10000) {
-    	var width = viewportWidth - viewportHeight;
-		cardSelector.css('width', width+"px");
-		cardSelector.css('height', "100vh");
-    } else if (viewportWidth/viewportHeight >= 7000/10000){
-		var height = viewportHeight - viewportWidth;
-		cardSelector.css('height', height+"px");
-		cardSelector.css('width', "100vw");
-    } else {
-		var height = viewportHeight - viewportWidth;
-		cardSelector.css('height', height+"px");
-		cardSelector.css('width', "100vw");
-	}
-}
-
 function showCard(){
 	var currentlyVisibleCard = document.getElementsByClassName('card selectedCard');
 	var newlyVisibleCard = document.getElementById(this.id + 'Card');
@@ -35,11 +12,8 @@ function addEventListeners() {
 	Array.prototype.forEach.call(selectors, function(selector){
 		selector.addEventListener("click", showCard);
 	});
-	window.addEventListener("orientationchange", sizeSelector);
-	window.addEventListener("resize", sizeSelector);
 }
 
 function runPostLoadFunctions(){
-	sizeSelector();
 	addEventListeners();
 }
