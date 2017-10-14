f17591d29feb97334ec00b39da6211de03d51d23
Ed Smilovici <edsmilovici@gmail.com>
2017-10-14 11:55:58 -0400

    Refs #15 using forward and back buttons updates the url but changes are not reflected by the website - added pop state event listener that triggers a card update without pushing a state that way the card is switched without refreshing the page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function showCard(){
+function showCard(pushState = true){
 	var currentlyVisibleCard = document.getElementsByClassName('card selectedCard');
 	var newlyVisibleCard = document.getElementById(this.id + 'Card');
 	Array.prototype.forEach.call(currentlyVisibleCard, function(card){
 		card.classList.remove('selectedCard');
 	});
 	newlyVisibleCard.classList.add('selectedCard');
-	history.pushState({}, '', '?card='+this.id);
+	if (pushState){
+		history.pushState({}, '', '?card='+this.id);
+	}
 }
 
 function showNextCard(){
@@ -38,6 +40,16 @@ function showNextCard(){
 	history.pushState({}, '', '?card='+cards[newlyVisibleCardIndex].id.slice(0, -4));
 }
 
+function selectCardBasedOnUrl(pushState = true) {
+    var url = window.location.href;
+    name = "card";
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results || !results[2]) return null;
+	var selectedCard = decodeURIComponent(results[2].replace(/\+/g, " "));
+	showCard.call({id : selectedCard}, pushState);
+}
+
 function addEventListeners() {
 	var selectors = document.getElementsByClassName('selector');
 	var sideBars = document.getElementsByClassName('sideBar');
@@ -47,16 +59,9 @@ function addEventListeners() {
 	Array.prototype.forEach.call(sideBars, function(sideBar){
 		sideBar.addEventListener("click", showNextCard);
 	});
-}
-
-function selectCardBasedOnUrl() {
-    var url = window.location.href;
-    name = "card";
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results || !results[2]) return null;
-	var selectedCard = decodeURIComponent(results[2].replace(/\+/g, " "));
-	showCard.call({id : selectedCard});
+	window.onpopstate = function(e) {
+		selectCardBasedOnUrl(false);
+	}
 }
 
 function runPostLoadFunctions(){
